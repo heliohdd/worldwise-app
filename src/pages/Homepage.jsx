31be9384a2e5c6a0111dcb132ff0d42e74b1d64b
Helio Dourado <heliohdd@yahoo.com.br>
2024-12-13 17:18:24 -0300

    Section 17.209 - Using CSS Modules

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
 import PageNav from "../components/PageNav";
+import AppNav from "../components/AppNav";
 
 function Homepage() {
   return (
     <div>
       <PageNav />
+      <AppNav />
       <h1>Homepage</h1>
-      <Link to={"/pricing"}>Princing</Link>
+      <Link to={"/app"}>Go to the app</Link>
     </div>
   );
 }
