e2b14263be05406203ad274248927be34c1a3559
Helio Dourado <heliohdd@yahoo.com.br>
2024-12-15 10:38:12 -0300

    Section 17.216 - Dynamic Routes With URL Parameters

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styles from "./CityItem.module.css";
 
 const formatDate = (date) =>
@@ -11,7 +12,7 @@ const formatDate = (date) =>
 /* eslint-disable react/prop-types */
 function CityItem({ city }) {
   console.log(city);
-  const { cityName, emoji, date } = city;
+  const { cityName, emoji, date, id } = city;
 
   const flagemojiToPNG = (flag) => {
     var countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt())
@@ -23,11 +24,13 @@ function CityItem({ city }) {
   };
 
   return (
-    <li className={styles.cityItem}>
-      <span className={styles.emoji}>{flagemojiToPNG(emoji)}</span>
-      <h3 className={styles.name}>{cityName}</h3>
-      <time className={styles.date}>{formatDate(date)}</time>
-      <button className={styles.deleteBtn}>&times;</button>
+    <li>
+      <Link className={styles.cityItem} to={`${id}`}>
+        <span className={styles.emoji}>{flagemojiToPNG(emoji)}</span>
+        <h3 className={styles.name}>{cityName}</h3>
+        <time className={styles.date}>{formatDate(date)}</time>
+        <button className={styles.deleteBtn}>&times;</button>
+      </Link>
     </li>
   );
 }
